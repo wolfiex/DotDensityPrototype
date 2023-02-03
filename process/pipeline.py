8f657fd364b96bf378103a321db69c4fa1123d28
Daniel <daniel.ellis.professional+git@gmail.comcd>
2023-02-03 00:48:12 +0000

    makefile

--- process/pipeline.py
+++ process/pipeline.py
@@ -247,7 +247,7 @@ if __name__ == '__main__':
 
     # from memory_profiler import profile
     # @profile
-    def gunwale_bobbing(schema,it=0,subset=False):
+    def gunwale_bobbing(schema,it=0,subset=[]]):
         
         x,y,z = schema
 
